21a67ff08e16c8acf1775029e247e65e9957f49f
Michael Owens <mowens@ucdavis.edu>
2015-05-19 14:45:49 -0700

    user login with tradecrafted.com email is required to view the app.

---
+++ app.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var uid2 = require('uid2');
+var router = express.Router();
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+var app = express();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+var GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+var GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+passport.use(new GoogleStrategy({  
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: GOOGLE_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, done) {
+      // console.log(profile)
+      console.log("GoogleStrategy done callback")
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
+));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  genid: function(req) {
+    return uid2(32); // use UUIDs for session IDs
+  },
+  secret: process.env.SESSION_SECRET
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/', routes);
+
+//authenticate
+app.use('/auth/google', passport.authenticate('google',  
+    { scope: ['https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read'] }),
+    function(req, res){} // this never gets called
+);
+
+//redirect after authenticate
+app.use('/oauth2callback',
+  passport.authenticate('google', { failureRedirect: '/login_fail'}),
+  function(req, res) {
+    if (req.user['_json'].domain == "tradecrafted.com")
+      res.redirect('/mentors');
+    else {
+      res.redirect('/login_fail');
+    }
+  }
+);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+passport.serializeUser(function(user, done) {  
+    console.log("serlializing user");
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, callback){
+       console.log('deserialize user.');
+       callback(null, user);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,142 @@
+var express = require('express');
+var router = express.Router();
+var GoogleSpreadsheet = require("google-spreadsheet");
+var _ = require('underscore');
+var moment = require('moment');
+
+var allTrackSlotsPage = 7;
+var trackSlotPage = 6;
+var engPage = 5;
+var growthPage = 4;
+var salesPage = 3;
+var pdPage = 2;
+var allTrackPage = 1;
+
+//the google sheet we're using
+var my_sheet = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+
+my_sheet.setAuth(process.env.EMAIL, process.env.GOOGLE_PASSWORD, function() {});	
+
+/* GET pages. */
+router.get('/mentors/eng', ensureAuthenticated, function(req, res, next) {
+		// spreadsheet key is the long id in the sheets URL 
+		my_sheet.getRows(trackSlotPage, function(err, allSlots) {
+			getAllSlots(err, allSlots, engPage, res, "eng");
+
+		});
+});
+
+router.get('/mentors/pd', ensureAuthenticated, function(req, res, next) {
+		// spreadsheet key is the long id in the sheets URL 
+		my_sheet.getRows(trackSlotPage, function(err, allSlots) {
+			getAllSlots(err, allSlots, pdPage, res, "pd");
+
+		});
+});
+
+router.get('/mentors/sales', ensureAuthenticated, function(req, res, next) {
+		// spreadsheet key is the long id in the sheets URL 
+		my_sheet.getRows(trackSlotPage, function(err, allSlots) {
+			getAllSlots(err, allSlots, salesPage, res, "sales");
+
+		});
+});
+
+router.get('/mentors/growth', ensureAuthenticated, function(req, res, next) {
+		// spreadsheet key is the long id in the sheets URL 
+		my_sheet.getRows(trackSlotPage, function(err, allSlots) {
+			getAllSlots(err, allSlots, growthPage, res, "growth");
+
+		});
+});
+
+router.get('/mentors/', ensureAuthenticated, function(req, res, next) {
+		// spreadsheet key is the long id in the sheets URL 
+		my_sheet.getRows(allTrackSlotsPage, function(err, allSlots) {
+			getAllSlots(err, allSlots, allTrackPage, res, "all");
+		});
+});
+
+///////authentication routes////////////
+
+//login page
+router.get('/login', function(req, res, next) {
+  res.send('Welcome! <a href="/auth/google"> Login with Google </a>')
+});
+
+//login fail route
+router.get('/login_fail', function(req, res){
+  res.send('login failed! please <a href="/auth/google">log in using your tradecrafted email</a>');
+});
+
+
+//logout
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/logged_out');
+});
+
+router.get('/logged_out', function(req, res){ 
+	res.send('Logged out!');
+});
+
+function ensureAuthenticated(req, res, next) {  
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/auth/google');
+}
+
+//////////////////////////////////////////////////////////////
+
+function getAllSlots(err, allSlots, responsePage, res){
+		var trackTimeSlots = [];
+
+		//grab all the date/time slots from the original spreadsheet
+		for ( var i = 0; i < allSlots.length; i++){
+			var time = new Date(allSlots[i]['datetime']);
+			if (new Date() < time) {
+				trackTimeSlots.push(moment(time).format('llll'));
+			}
+		}
+
+		var options = {
+			orderby: "datetime"
+		}
+
+		my_sheet.getRows(responsePage, options, function(err, allMentors){
+			getMentorSlotObject(trackTimeSlots, allMentors, res);
+		});
+}
+
+function getMentorSlotObject(trackTimeSlots, allMentors, res){
+
+		//ordered list of mentors
+
+		var pastMentors = [];
+		var upcoming = [];
+
+		for(var i = 0; i < allMentors.length; i++){
+			var mentorTime = new Date(allMentors[i]['datetime']);
+
+			//reformat date to be prettier
+			allMentors[i]['datetime'] = moment(mentorTime).format('llll');
+			
+			var slotIndex = trackTimeSlots.indexOf(mentorTime.toString());
+
+			//separate mentors by past and upcoming
+			if (mentorTime > new Date()) {
+				upcoming.push(allMentors[i]);
+			}
+			else {
+				pastMentors.push(allMentors[i]);
+			}
+
+			//remove taken slots from list of slots
+			if (slotIndex > -1){
+			 	trackTimeSlots.splice(slotIndex, 1);
+			}
+		}
+
+		res.render('index', { title: 'Mentos' , upcoming: upcoming, slots: trackTimeSlots.reverse(), past: pastMentors.reverse()});
+}
+
+module.exports = router;
