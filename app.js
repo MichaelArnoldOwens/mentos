c0b6e0ddc12be6e1c99a2d776bec20135ae677b1
Michael Owens <mowens@ucdavis.edu>
2015-05-20 10:56:57 -0700

    should work off cloning and npm install now

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
+  saveUninitialized: true,
   genid: function(req) {
     return uid2(32); // use UUIDs for session IDs
   },
